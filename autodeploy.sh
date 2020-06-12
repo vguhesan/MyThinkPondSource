49890770ba8897163d78808fa9487d02329e0d5a
Venkatt Guhesan <vguhesan@gmail.com>
2020-06-12 00:56:26 -0400

    Latest Site Updated Commit From: 20200612

---
+++ autodeploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+mkdir -p ./public/
+hugo --theme=mainroad
+npm run index
+firebase deploy
+git add .
+outfile="Latest Site Updated Commit From: $(date +%Y%m%d)"
+git commit -m "${outfile}"
+git push -u origin develop
+pwd
+
